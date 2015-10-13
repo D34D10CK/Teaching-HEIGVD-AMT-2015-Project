b0b5f916b5af85e27031339e06cf52c2efbee391
Sacha Bron <sacha.bron@copperflake.com>
2015-10-13 14:29:35 +0200

    Remove Mock DB

--- AMTProject/src/main/java/ch/heigvd/amt/amtproject/services/AppsManager.java
+++ AMTProject/src/main/java/ch/heigvd/amt/amtproject/services/AppsManager.java
@@ -10,29 +10,9 @@ import java.util.logging.Logger;
 
 @Stateless
 public class AppsManager implements AppsManagerLocal {
-	private static final int MAX_DATABASE_DELAY_MS = 0;
-	//private static final int MAX_DATABASE_DELAY_MS = 10000;
-	@EJB
-	AppsDataStoreLocal appsDataStore;
-
-	/**
-	 * This method returns the list of all beers in the data store.
-	 * @return a list of beers
-	 */
 	@Override
 	public List<Application> getAllApps() {
-		simulateDatabaseDelay();
-		return appsDataStore.getAllApps();
+		// TODO Query DB
+		return null;
 	}
-
-	private void simulateDatabaseDelay() {
-		long simulatedDatabaseLookupTime = (long)(Math.random() * MAX_DATABASE_DELAY_MS);
-		try {
-			Thread.sleep(simulatedDatabaseLookupTime);
-		} catch (InterruptedException ex) {
-			Logger.getLogger(AppsManager.class.getName()).log(Level.SEVERE, null, ex);
-		}
-
-	}
-
 }
