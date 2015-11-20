7e51f04fa651a4fff381f1e124bc3ca3ea39a3bb
Sacha Bron <sacha.bron@copperflake.com>
2015-11-20 11:16:16 +0100

    Clean API-doc

--- specifications/api-doc.java
+++ specifications/api-doc.java
@@ -1,8 +1,10 @@
 // Generate with: `apidoc -i specifications -o apidoc`
+
+// Badges
 /**
  * @api {GET} /badges Get every badges
  * @apiName GetBadges
- * @apiGroup badges
+ * @apiGroup Badges
  * @apiVersion 0.1.0
  *
  * @apiSuccessExample Success-Response:
@@ -12,60 +14,53 @@
  *	}]
  * 
  */
+
+
 /**
  * @api {POST} /badges Add a new badge
  * @apiName PostBadges
- * @apiGroup badges
+ * @apiGroup Badges
  * @apiVersion 0.1.0
  *
  * @apiParam {String} name Name of the Badge
  *
  * @apiSuccessExample Success-Response:
- *     HTTP/1.1 201 Created
+ *  HTTP/1.1 201 Created
  * 
  */
+
+
 /**
  * @api {PUT} /badges/:id Update a badge
  * @apiName PutBadges
- * @apiGroup badges
+ * @apiGroup Badges
  * @apiVersion 0.1.0
  *
  * @apiParam {Number} id Badge unique ID.
  * @apiParam {String} name Name of the Badge
  *
  * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *     "Badge updated"
+ *  HTTP/1.1 200 OK
+ *  "Badge updated"
  * 
  * 
  * @apiErrorExample Error-Response:
- *     HTTP/1.1 404 Not Found
+ *  HTTP/1.1 404 Not Found
  */
+
+
 /**
  * @api {DELETE} /badges/:id Delete a badge
  * @apiName DeleteBadges
- * @apiGroup badges
+ * @apiGroup Badges
  * @apiVersion 0.1.0
  *
  * @apiParam {Number} id Badge unique ID.
  *
  * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *     "Badge deleted"
+ *  HTTP/1.1 200 OK
+ *  "Badge deleted"
  * 
  * @apiErrorExample Error-Response:
- *     HTTP/1.1 404 Not Found
- */
-/**
- * @api {GET} /badges/:id Get a single badge
- * @apiName GetBadge
- * @apiGroup badges
- * @apiVersion 0.1.0
- *
- * @apiSuccessExample Success-Response:
- *   HTTP/1.1 200 OK
- *   {
- *     "name": "CoolBadge"
- *   }
- * 
+ *  HTTP/1.1 404 Not Found
  */
