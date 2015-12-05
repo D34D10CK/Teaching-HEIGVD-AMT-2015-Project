e3b2676fa4726ac237c5c0b51deed19c9c832d82
Sacha Bron <sacha.bron@copperflake.com>
2015-12-05 20:00:03 +0100

    Add Meteor Methods to call the API

--- demo/admin/badges/badges.js
+++ demo/admin/badges/badges.js
@@ -36,13 +36,18 @@ if (Meteor.isServer) {
 	API_URL = "http://localhost:8080/AMTProject/api/";
 	API_KEY = '8b628357-9e5f-4008-a117-557e623637c4';
 
-	HTTP.call('GET', API_URL + "badges", {
-		headers: {
-			'api-key': API_KEY
+	Meteor.methods({
+		refreshBadges: function () {
+			HTTP.call('GET', API_URL + "badges", {
+				headers: {
+					'api-key': API_KEY
+				}
+			},
+			function (err, result) {
+				console.info(err);
+				console.info(result);
+				console.log(API_URL + "badges");
+			});
 		}
-	},
-	function (err, result) {
-		console.info(err);
-		console.info(result);
 	});
 }
