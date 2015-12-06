e42e797a93f44699ea045de2b6460ab8125cc832
Sacha Bron <sacha.bron@copperflake.com>
2015-12-06 02:22:48 +0100

    Add Test Badges

--- AMTProject/src/main/java/ch/heigvd/amt/amtproject/services/TestDataManager.java
+++ AMTProject/src/main/java/ch/heigvd/amt/amtproject/services/TestDataManager.java
@@ -1,12 +1,15 @@
 package ch.heigvd.amt.amtproject.services;
 
 import ch.heigvd.amt.amtproject.model.entities.*;
+import ch.heigvd.amt.amtproject.rest.dto.BadgeCreationDTO;
 import ch.heigvd.amt.amtproject.services.dao.ApplicationDAOLocal;
 import ch.heigvd.amt.amtproject.services.dao.UserDAOLocal;
 
 import ch.heigvd.amt.amtproject.services.dao.ApiKeyDAOLocal;
 import ch.heigvd.amt.amtproject.services.dao.EndUserDAOLocal;
 import ch.heigvd.amt.amtproject.services.dao.RoleDAOLocal;
+import ch.heigvd.amt.amtproject.services.dao.rest.BadgeDAO;
+import ch.heigvd.amt.amtproject.services.dao.rest.BadgeDAOLocal;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -25,9 +28,12 @@ public class TestDataManager implements TestDataManagerLocal {
     
     @EJB
     RoleDAOLocal roleDAO;
- 
-    @EJB
-    EndUserDAOLocal enduUserDAO;
+
+	@EJB
+	EndUserDAOLocal enduUserDAO;
+
+	@EJB
+	BadgeDAOLocal badgeDAOLocal;
 
     @Override
     public void generateTestData() {
@@ -37,7 +43,7 @@ public class TestDataManager implements TestDataManagerLocal {
         userDAO.create(u);
         userDAO.create(u2);
         
-        ApiKey key1 = new ApiKey();
+        ApiKey key1 = new ApiKey("c45e61bf-dbf1-4404-bf6d-7fcbb83d4d26");
         ApiKey key2 = new ApiKey();
         ApiKey key3 = new ApiKey();
         
@@ -60,7 +66,13 @@ public class TestDataManager implements TestDataManagerLocal {
             createEndUser(a1);
             createEndUser(a2);
         }
-           
+
+		Badge b1 = new Badge("Test1", "https://playfoursquare.s3.amazonaws.com/badge/300/local.png", a1);
+		Badge b2 = new Badge("Test2", "https://playfoursquare.s3.amazonaws.com/badge/300/newbie.png", a1);
+
+		badgeDAOLocal.create(b1);
+		badgeDAOLocal.create(b2);
+
         //UserRole r1 = new UserRole("admin");
         //UserRole r2 = new UserRole("developper");
         
