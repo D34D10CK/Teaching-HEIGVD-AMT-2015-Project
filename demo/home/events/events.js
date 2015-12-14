8b5c8f02a4d8ba911f5666ec44e493bbff7a710c
Sacha Bron <sacha.bron@copperflake.com>
2015-12-14 11:20:46 +0100

    Init gamification app

--- demo/home/events/events.js
+++ demo/home/events/events.js
@@ -4,8 +4,53 @@ if (Meteor.isClient) {
 			return Users.find({});
 		}
 	});
+
+	Template.eventsTemplate.events({
+		'click #send-event-button': function (e) {
+			var user = document.querySelector('#send-event-user').value;
+			var eventName = document.querySelector('#send-event-event').value;
+
+			console.log(user, eventName);
+
+			Meteor.call("sendEvent", user, eventName);
+		}
+	});
 }
 
 if (Meteor.isServer) {
-	
+	Meteor.methods({
+		sendEvent: function (user, eventName) {
+			HTTP.post(API_URL + "events", {
+				headers: {
+					'apiKey': API_KEY
+				}, data: {
+					eventName: eventName,
+					userId: user,
+					eventDate: new Date().getTime() / 1000,
+					conditions: {
+						difficulty: "hard"
+					}
+				}
+			},
+			function (err, result) {
+				if (eventName == 1) {
+					Users.update({ id: user }, { $inc: { points: 20 } });
+				} else {
+					Users.update({ id: user }, { $push: { badges: Badge.findOne() } });
+				}
+				
+				Meteor.call("refreshReputation", user)
+			});
+		},
+		refreshReputation: function (user) {
+			HTTP.get(API_URL + "reputation/" + user, {
+				headers: {
+					'apiKey': API_KEY
+				}
+			},
+			function (err, result) {
+				console.log(result);
+			});
+		}
+	});
 }
